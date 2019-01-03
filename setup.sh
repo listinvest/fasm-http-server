6049449b7f52e80361d01fe387ee82a5274b743e
Nadeen Udantha <udanthan@gmail.com>
2019-01-03 17:11:31 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-wget --no-verbose https://flatassembler.net/fasm-1.73.04.tgz --output-document=/tmp/fasm.tgz
-tar -xzf /tmp/fasm.tgz -C $PWD
-chmod +x fasm/fasm
-export PATH="$PATH:$PWD/fasm"
-fasm main.asm
+wget --no-verbose https://github.com/NadeenUdantha/heroku-buildpack-fasm/raw/master/bin/fasm
+chmod +x fasm
+fasm main.asm main
