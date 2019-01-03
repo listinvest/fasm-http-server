3aa7b45009124122ad8d0e48fddf47b5c32be1dd
Nadeen Udantha <udanthan@gmail.com>
2019-01-03 17:44:50 +0530

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,6 +1,6 @@
-;fasm-server
-;heroku fasm server
-;Copyright © 2018 Nadeen Udantha
+;fasm-http-server
+;fasm http server
+;Copyright © 2019 Nadeen Udantha
 ;<udanthan@gmail.com>
 ;This program is free software: you can redistribute it and/or modify 
 ;it under the terms of the GNU General Public License as published by
@@ -16,38 +16,23 @@
 format ELF executable 3
 entry main
  
-segment readable writable executable
+segment readable writeable executable
 
 main:
         pop     eax
         pop     ebx
         pop     ecx
-        mov     edx,ecx
+        mov     esi,ecx
         push    ecx
         push    ebx
         push    eax
-        mov     eax,dword[edx]
-        mov     bx,word[edx+4]
-        mov     dword[srv_portz],eax
-        mov     word[srv_portz+4],bx
-        mov     esi,srv_portz
-        call    strz.puts
-        mov     esi,str_endl
-        call    strz.puts
-        mov     esi,srv_portz
         call    atoi
         mov     dword[srv_port],eax
         call    so.socket
-        mov     dword[sso],eax
-        mov     esi,str_001
-        call    strz.puts
+        mov     dword[sss],eax
         mov     ebx,dword[srv_port]
         call    so.bind
-        mov     esi,str_002
-        call    strz.puts
         call    so.listen
-        mov     esi,str_003
-        call    strz.puts
 .xc:
         call    so.accept
         mov     esi,str_004
@@ -60,6 +45,9 @@ main:
         int     0x80
 .e:
         jmp     .e
+
+srv_port dd 0
+sss dd ?
 
 so:
 .sd1: dd 2,1,0
@@ -167,12 +155,3 @@ atoi:
         pop     edx
         pop     ecx
         ret
-
-srv_portz rb 6
-srv_port dd 0
-sso dd ?
-str_endl db 0x0a,0
-str_001 db 'str_001',0xA,0
-str_002 db 'str_002',0xA,0
-str_003 db 'str_003',0xA,0
-str_004 db 'str_004',0xA,0
